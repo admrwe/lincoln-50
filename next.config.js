c663dc01c8365ab7fef786c03cbf35d101874149
Adam Rowe <admrwe@Adams-MBP.gpcom.net>
2023-06-16 14:38:19 -0600

    base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/lincoln-50/",
+  basePath: "/lincoln-50",
   experimental: {
     appDir: true,
   },
