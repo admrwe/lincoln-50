86d69ae67d134494390ca9523cef8ea3bd63e564
Adam Rowe <admrwe@Adams-MBP.gpcom.net>
2023-06-16 14:22:36 -0600

    Revert next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  // basePath: "/lincoln-50",
-  // experimental: {
-  //   appDir: true,
-  // },
+  basePath: "/lincoln-50",
+  experimental: {
+    appDir: true,
+  },
 };
 
 module.exports = nextConfig;
